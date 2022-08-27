a844d729bfe6e66c800fb9026e227f62040a7bb2
Yarooq Anwar <yarooq1@gmail.com>
2022-08-27 23:39:17 +0500

    selective repeat almost done

--- server.js
+++ server.js
@@ -10,19 +10,56 @@ const io = new Server(server);
 
 io.on('connection', (socket) => {
     console.log('a user connected');
-    // setInterval(() => {
-    //     io.emit('hello', 'packet')
-    // },5000)
-    io.emit('user', 'userIsDead');
+    socket.on('startServer', () => {
+        io.emit('initPacket', 'initPacket');
+    })
 
     socket.on('disconnect', () => {
         console.log('user disconnected');
-      });
+    });
 
-    socket.on('clientmsg', (msg) => {
+    socket.on('clientMsg', (msg) => {
         console.log('msg: ' + msg);
         io.emit('msg', msg);
     });
+
+    socket.on('clientAck1', (msg) => {
+        console.log(msg)
+        io.emit('serverPacket', 'Packet');
+    })
+
+    socket.on('hi', (msg) => {
+        var count = 1;
+        
+        function packetDelay(msg){
+            setTimeout(()=>{
+                // console.log(msg);
+                io.emit('packetR', 'packet ' + count);
+                count++;
+                if(count <= msg){
+                    packetDelay(msg);
+                }
+            },3000)
+        }
+        packetDelay(msg);
+        
+        // for (let i = 1; i <= msg; i++) {
+            // console.log(i); 
+            // console.log('msg:' + msg); 
+            // setTimeout(() => {
+            //     console.log(msg);
+            //     io.emit('packetR', 'packet ' + i);                                
+                // if(i == msg) {
+                //     // clearInterval();
+                // } else {
+                // }
+            // },3000)
+        // }
+    })
+
+    // socket.on('clientAck', (msg) => {
+    //     io.emit('serverPacket', 'Packet');
+    // })
 });
 
 // const io = new Server(app);
@@ -32,7 +69,7 @@ app.get('/', (req, res) => {
 })
 
 server.listen(3000, () => {
-    console.log("sad")
+    console.log("Server is Running on 3000");
 });
 
 // console.log("nice");
