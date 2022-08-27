bf1260689e16a2b9dc241d51ef9b293f606634dc
Yarooq Anwar <yarooq1@gmail.com>
2022-08-27 20:15:07 +0500

    client and server

--- server.js
+++ server.js
@@ -3,39 +3,32 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
+const { setInterval } = require('timers');
 const io = new Server(server);
 
 // var io = require('socket.io')(3000)
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+    // setInterval(() => {
+    //     io.emit('hello', 'packet')
+    // },5000)
     io.emit('user', 'userIsDead');
 
     socket.on('disconnect', () => {
         console.log('user disconnected');
       });
 
-    socket.on('msg', (msg) => {
+    socket.on('clientmsg', (msg) => {
         console.log('msg: ' + msg);
         io.emit('msg', msg);
     });
-
-    // io.emit('newMsg', msg+"hard");
-    // app.send('Dead')
-    // socket.emit('message', 'Hello world');
-    // socket.on('disconnect', () => {
-    //     console.log('user disconnected');
-    // });    
 });
 
 // const io = new Server(app);
 
 app.get('/', (req, res) => {
     res.sendFile('E:/Just Some Files/C#/index.html');
-    // setInterval(() => {
-    //     res.send('olo');
-    // },1000)
-    // res.sendFile('E:/Just Some Files/C#/main.js');
 })
 
 server.listen(3000, () => {
